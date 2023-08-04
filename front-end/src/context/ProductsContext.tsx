5dbc8304ad7f05009d0812f4c965f21c32b32670
Khrystyna Derenivska <khrisderenivska@gmail.com>
2023-08-04 22:20:24 +0300

    get products data by pages

--- front-end/src/context/ProductsContext.tsx
+++ front-end/src/context/ProductsContext.tsx
@@ -1,33 +1,50 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import { ProductItem } from "../../mock-tool/Product";
+import { PageObject } from "../../mock-tool/PageObject";
 
 type ProductsState = {
-  products: Array<ProductItem>;
+  products: PageObject<ProductItem>;
   isLoading: boolean;
   error?: Error;
 };
 
+const initialProductsState = {
+  data: [],
+  limit: null,
+  total: null,
+  currentPage: null,
+};
+
 export const ProductsContext = createContext<ProductsState>({
-  products: [],
+  products: initialProductsState,
   isLoading: true,
   error: null,
 });
 
 export function ProductsContextProvider({ children }: { children: ReactNode }) {
   const [products, setProducts] = useState<ProductsState>({
-    products: [],
+    products: initialProductsState,
     isLoading: false,
     error: null,
   });
 
   useEffect(() => {
-    setProducts({ products: [], isLoading: true, error: null });
+    setProducts({
+      products: initialProductsState,
+      isLoading: true,
+      error: null,
+    });
 
     fetch("http://localhost:3000/products")
       .then((response) => response.json())
-      .then((products) =>
-        setProducts({ products, isLoading: false, error: null })
-      )
+      .then((productsPage: PageObject<ProductItem>) => {
+        productsPage.data = products.products.data.concat(productsPage.data);
+        setProducts({
+          products: productsPage,
+          isLoading: false,
+          error: null,
+        });
+      })
       .catch((error) => setProducts((prevState) => ({ ...prevState, error })));
   }, []);
 
